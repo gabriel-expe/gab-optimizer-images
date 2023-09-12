097672cfdb150a7f3712fc256a516d8d864a9de7
Gabriel Rubio <gabrielrubio983@gmail.com>
2023-09-12 11:30:40 -0500

    fix --help command to be more clear

--- bin/index.js
+++ bin/index.js
@@ -6,17 +6,21 @@ import { processDirectory } from '../lib/process-directory.js';
 import { argv } from '../config/config.js';
 
 const path = argv._[1];
-console.log(path);
-lstat(path, (err, stats) => {
-	console.log(path);
-	if (err) {
-		console.log('Error reading stats from ', path);
-		process.exit(1);
-	}
+if (path) {
+	lstat(path, (err, stats) => {
+		console.log(path);
+		if (err) {
+			console.log('Error reading stats from ', path);
+			process.exit(1);
+		}
 
-	if (!stats.isDirectory()) {
-		processFile(path);
-	} else {
-		processDirectory(path);
-	}
-});
+		if (!stats.isDirectory()) {
+			processFile(path);
+		} else {
+			processDirectory(path);
+		}
+	});
+} else {
+	console.log('Missing command, you can try compress');
+	process.exit(1);
+}
--- config/config.js
+++ config/config.js
@@ -1,32 +1,42 @@
 import yargs from 'yargs/yargs';
 import { hideBin } from 'yargs/helpers';
 
-export const { argv } = yargs(hideBin(process.argv)).command({
-	command: 'compress',
-	describe: 'optimize images in the folder',
-	builder: {
-		resize: {
-			alias: 'r',
-			describe: 'Resize with maximum width in pixels',
-			type: 'number',
+const COLOR = '\x1b[36m';
+const RESET_COLOR = '\x1b[0m';
+
+export const { argv } = yargs(hideBin(process.argv))
+	.scriptName('gio')
+	.usage('$0 <cmd> [args]')
+	.command({
+		command: 'compress',
+		describe: `optimize images in the folder, use ${COLOR}gio compress --help${RESET_COLOR} to see parameters`,
+		builder: {
+			resize: {
+				alias: 'r',
+				describe: 'Resize with maximum width in pixels',
+				type: 'number',
+			},
+			quality: {
+				alias: 'q',
+				describe: 'Quality setting, default 70',
+				type: 'number',
+				default: 70,
+			},
+			format: {
+				alias: 'f',
+				describe: 'Output format for png/jpg/webp',
+				type: 'string',
+			},
+			destination: {
+				alias: 'd',
+				describe:
+					'Destination path to create the new images folder, by default is the folder where user is executing program',
+				type: 'string',
+				default: '.',
+			},
 		},
-		quality: {
-			alias: 'q',
-			describe: 'Quality setting, default 70',
-			type: 'number',
-			default: 70,
-		},
-		format: {
-			alias: 'f',
-			describe: 'Output format for png/jpg/webp',
-			type: 'string',
-		},
-		destination: {
-			alias: 'd',
-			describe:
-				'Destination path to create the new images folder, by default is the folder where user is executing program',
-			type: 'string',
-			default: '.',
-		},
-	},
-});
+	})
+	.help()
+	.alias('help', 'h')
+	.version()
+	.alias('version', 'v');
